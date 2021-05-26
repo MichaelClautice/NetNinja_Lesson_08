9078717b0c407ff6c38e2b9b4dafaf3c67596c73
Michael Clautice <michaelclauticeusa@gmail.com>
2021-05-26 15:09:09 -0400

    Mike modified 2 files Wed.5.26.21.

--- app.js
+++ app.js
@@ -3,13 +3,17 @@
 // Lesson 08 - The Node Event Emitter
 //-----
 
+// req events mod
 const events = require('events');
-
+// create an instantiation of events.EventEmitter()
 const myEmitter = new events.EventEmitter();
 
+// use th on( ) event handler to handle a custom event named mikesEvent
+// when it receives mikesEvent it will console.log a message
 myEmitter.on('mikesEvent', function(mssg){
     console.log(mssg);
 });
 
-myEmitter.emit('mikesEvent', 'Mikes event was emitted');
+// this statement emits a custom event w a mssg as an arg
+myEmitter.emit('mikesEvent', '\nMikes event was emitted!\n');
 
--- app2.js
+++ app2.js
@@ -3,26 +3,42 @@
 // Lesson 08 - The Node Event Emitter
 //-----
 
+// req events module
 const events = require('events');
+// req util module
 const util = require('util');
 
+// create a Person obj that will have a name
 var Person = function(name){
     this.name = name;
 };
 
+// util's inherits( ) method incs 2 params:
+// 1 - who will b inheriting;
+// obj cnstrctr Person
+// 2 - what it will b inheriting:
+// The event module’s EventEmitter */
 util.inherits(Person, events.EventEmitter);
 
-var joe = new Person('james');
-var mary = new Person('mary');
-var ryu = new Person('ryu');
-var people = [joe, mary, ryu];
+// create 3 instantiations of Person
+var Joe = new Person('Joe');
+var Mary = new Person('Mary');
+var Ryu = new Person('Ryu');
+// create an array of the Person objs
+var people = [Joe, Mary, Ryu];
 
+// for ea of th Person objs in th array...
 people.forEach(function(person){
+// that Person will emit a speak event and...
     person.on('speak', function(mssg){
+// that Person's mssg will be con.logged
+        console.log('\n');
         console.log(person.name + ' said: ' + mssg);
     })
 });
 
-joe.emit('speak', 'hello dudes');
-ryu.emit('speak', 'I want ice cream');
+// 2 Person objs emit custom events that will be acted on...
+// aalong w their str mssgs
+Joe.emit('speak', '\nHello dudes!');
+Ryu.emit('speak', '\nI want ice cream NOW!!\n');
 
